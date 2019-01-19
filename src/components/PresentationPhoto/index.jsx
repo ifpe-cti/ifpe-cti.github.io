cc489fa74fec943added37a654a027008d34da57
joelhenrique2000 <joelhenrique2000@gmail.com>
2019-01-19 00:47:31 -0200

    Criando o protótipo do componente PresentationPhoto

--- src/components/PresentationPhoto/index.jsx
+++ src/components/PresentationPhoto/index.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import '../../assets/css/reset.css'
-
-
+import { Container, GlobalStyle, BackgroundOpacity } from './styles'
+import campus from '../../assets/img/campus.jpg'
+import './style.css';
 export default class PresentationPhoto extends Component {
 
     constructor(props) {
@@ -10,9 +11,23 @@ export default class PresentationPhoto extends Component {
 
     render() {
         return (
-            <header>
-                <img src={this.props.src} alt="Campus do ifpe Garanhuns" />
-            </header>
+            <div>
+                <GlobalStyle/>
+                <Container>
+                    <BackgroundOpacity 
+                        opacity={0.7} 
+                        color={'#000'}
+                    />
+
+                    <div className="caption">
+                        <span className="border">IFPE - Campus Garanhuns</span><br/>
+                    </div>
+                </Container>
+
+                <p style={{padding:'5px 15px'}}>This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.</p>
+
+
+            </div>
         )
     }
 
--- src/components/PresentationPhoto/styles.js
+++ src/components/PresentationPhoto/styles.js
@@ -1,1 +1,50 @@
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+import campus from '../../assets/img/campus.jpg'
+
+export const GlobalStyle = createGlobalStyle`
+    body, html {
+    height: 100%;
+    margin: 0;
+    font: 400 15px/1.8 "Lato", sans-serif;
+    color: #777;
+    }
+`
+
+export const BackgroundOpacity = styled.div`
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: ${props => props.color};
+    opacity: ${props => props.opacity};
+`;
+
+export const Container = styled.div`
+
+    
+    background-image: url(${campus});
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+
+    .caption {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    width: 100%;
+    text-align: center;
+    color: #000;
+    }
+
+    .caption span.border {
+    background-color: #111;
+    color: #fff;
+    padding: 18px;
+    font-size: 25px;
+    letter-spacing: 10px;
+    }
+
+`;
