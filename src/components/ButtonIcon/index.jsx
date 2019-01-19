66e1387d9ba592dd109df2d619fad2d8adfd4e8e
joelhenrique2000 <joelhenrique2000@gmail.com>
2019-01-19 20:55:08 -0200

    Trocando o nome da classe de BottonIcon para ButtonIcon

--- src/components/ButtonIcon/index.jsx
+++ src/components/ButtonIcon/index.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { Button, Text } from './styles'
 
-export default class BottonIcon extends Component {
+export default class ButtonIcon extends Component {
     render() {
         return (
             <Button color={this.props.color} colorHover={this.props.colorHover}>
