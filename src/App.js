06f86b64938f9f06da1878890402dead4154309a
joelhenrique2000 <joelhenrique2000@gmail.com>
2019-01-18 22:41:48 -0200

    Iniciando a estrutura de pastas do projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PresentationPhoto from './components/PresentationPhoto'
+
+class App extends Component {
+  render() {
+    return (
+      <PresentationPhoto />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PresentationPhoto/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class PresentationPhoto extends Component {
+
+    render() {
+        return (
+            <h1>
+                asds
+            </h1>
+        )
+    }
+
+}
